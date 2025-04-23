74ed4288d6e0b4182d348500f6f3bae7574025a5
nairx <nairxprv@gmail.com>
2025-04-23 10:40:55 +0530

    Updated

--- q24.js
+++ q24.js
@@ -25,21 +25,54 @@ db.posts.insertMany([
   },
 ]);
 
-
 db.userPosts.insertMany([
-    {
-        title:"title 1",
-        desc:"desc 1",
-        name:"name 1"
+  {
+    title: "title 1",
+    desc: "desc 1",
+    name: "name 1",
+  },
+  {
+    title: "title 2",
+    desc: "desc 2",
+    name: "name 1",
+  },
+  {
+    title: "title 3",
+    desc: "desc 3",
+    name: "name 2",
+  },
+]);
+
+//write a query to display name and title
+// from userPosts
+
+db.userPosts.find(
+  {},
+  {
+    _id: 0,
+    name: 1,
+    title: 1,
+  }
+);
+
+db.userPosts.updateMany(
+  { name: "name 1" },
+  {
+    $set: { name: "n1" },
+  }
+);
+
+db.createCollection("cust", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["name", "email"],
     },
-    {
-        title:"title 2",
-        desc:"desc 2",
-        name:"name 1"
-    },
-    {
-        title:"title 3",
-        desc:"desc 3",
-        name:"name 2"
-    }
-])
+  },
+});
+
+db.cust.insertOne({
+    name:"customer 1",
+    email:"customer1@email.com",
+    city:"Hyd"
+})
